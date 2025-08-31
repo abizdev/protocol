53e736110890a9cd919c6526c5e3f44ada2f359c
Abdulaziz <abizlancer@gmail.com>
2025-08-31 18:35:36 +0500

    lint rules disable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@next/next/no-img-element': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off'
+    }
+  }),
   {
     ignores: [
       "node_modules/**",
